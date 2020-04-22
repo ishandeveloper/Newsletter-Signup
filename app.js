2f12e5c3ebf4dba31be26401fe6d4c7654b80249
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-22 15:02:21 +0530

    body parser

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var port = process.env.PORT || 8080;
 
 const app=express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/assets', express.static('assets'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/views/signup.html");
 });
+
+app.post('/');
 
 app.listen(port,()=>{
     console.log("Server Up At "+port)
