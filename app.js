c90b7c2e46ae407f236bbffd773f142dc281158b
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-22 16:33:39 +0530

    API_REQUEST

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const requrest = require('request');
+const request = require('request');
 const bodyParser = require("body-parser");
+require('dotenv').config();
+
 var port = process.env.PORT || 8080;
 
 const app=express();
-
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/assets', express.static('assets'));
@@ -16,8 +17,24 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     var name=req.body.fName;
     var email=req.body.Email;
-    console.log(email);
-    console.log(name);
+    var url="https://us4.api.mailchimp.com/3.0/lists/"+process.env.UNIQUE_ID;
+
+    var mailchimp_req={
+        url: url,
+        method:"POST",
+        headers:{
+            "Authorization":"ishandeveloper "+process.env.API_KEY
+        }
+    };
+    request(mailchimp_req,(e,res,body)=>{
+        if(e){
+            console.log(e);
+        }
+        else{
+            console.log(res.statusCode);
+        }
+    });
+
 });
 
 app.listen(port,()=>{
